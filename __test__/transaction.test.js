774f5cad328bcdf8879356d54dceb7ffff4806af
Chris Cooksley <christopher.cooksley@gmail.com>
2018-12-03 13:57:57 +0000

    Refactor transaction to have debit and credit properties

--- __test__/transaction.test.js
+++ __test__/transaction.test.js
@@ -3,33 +3,36 @@ import Transaction from '../src/transaction';
 describe('Transaction', () => {
   describe('when initialized with positive value of 500', () => {
     const transaction = new Transaction(500);
-    test('has value of 500', () => {
-      expect(transaction.value).toBe(500);
+
+    it('has credit of 500', () => {
+      expect(transaction.credit).toBe(500);
     });
-    test('has type of "credit"', () => {
-      expect(transaction.type).toBe('credit');
+    it('has debit of 0', () => {
+      expect(transaction.debit).toBe(0);
     });
-    test('defaults to current date', () => {
+    it('defaults to current date', () => {
       expect(transaction.date.toDateString()).toBe(new Date().toDateString());
     });
   });
   describe('when initialized with negative value of -500', () => {
     const transaction = new Transaction(-500);
-    test('has value of -500', () => {
-      expect(transaction.value).toBe(-500);
+    it('has debit of 500', () => {
+      expect(transaction.debit).toBe(500);
     });
-    test('has type of "debit"', () => {
-      expect(transaction.type).toBe('debit');
+    it('has credit of 0', () => {
+      expect(transaction.credit).toBe(0);
     });
   });
   describe('when initialized with value of 0', () => {
-    test('raises error', () => {
+    it('raises error', () => {
       const invalidTransaction = () => new Transaction(0);
       expect(invalidTransaction).toThrow('Invalid transaction');
     });
   });
   describe('when initialized with date', () => {
-    const transaction = new Transaction(500, '2018-12-25');
-    expect(transaction.date).toEqual(new Date('2018-12-25'));
+    it('has date given to it', () => {
+      const transaction = new Transaction(500, '2018-12-25');
+      expect(transaction.date).toEqual(new Date('2018-12-25'));
+    });
   });
 });
--- src/transaction.js
+++ src/transaction.js
@@ -1,8 +1,8 @@
 export default class Transaction {
   constructor(value, date = new Date()) {
     if (value === 0) throw new Error('Invalid transaction.');
-    this.value = value;
-    this.type = this.value >= 0 ? 'credit' : 'debit';
+    this.credit = value >= 0 ? Math.abs(value) : 0;
+    this.debit = value < 0 ? Math.abs(value) : 0;
     this.date = new Date(date);
   }
 }
