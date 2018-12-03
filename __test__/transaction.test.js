69b2bad6aa01d5408e0a2f4f9564afca5570d262
Chris Cooksley <christopher.cooksley@gmail.com>
2018-12-03 12:34:06 +0000

    Add date functionality to transactions

--- __test__/transaction.test.js
+++ __test__/transaction.test.js
@@ -9,5 +9,27 @@ describe('Transaction', () => {
     test('has type of "deposit"', () => {
       expect(transaction.type).toBe('deposit');
     });
+    test('defaults to current date', () => {
+      expect(transaction.date.toDateString()).toBe(new Date().toDateString());
+    });
+  });
+  describe('when initialized with negative value of -500', () => {
+    const transaction = new Transaction(-500);
+    test('has value of -500', () => {
+      expect(transaction.value).toBe(-500);
+    });
+    test('has type of "withdrawal"', () => {
+      expect(transaction.type).toBe('withdrawal');
+    });
+  });
+  describe('when initialized with value of 0', () => {
+    test('raises error', () => {
+      const invalidTransaction = () => new Transaction(0);
+      expect(invalidTransaction).toThrow('Invalid transaction');
+    });
+  });
+  describe('when initialized with date', () => {
+    const transaction = new Transaction(500, '2018-12-25');
+    expect(transaction.date).toEqual(new Date('2018-12-25'));
   });
 });
--- src/transaction.js
+++ src/transaction.js
@@ -1,6 +1,8 @@
 export default class Transaction {
-  constructor(value) {
+  constructor(value, date = new Date()) {
+    if (value === 0) throw new Error('Invalid transaction.');
     this.value = value;
     this.type = this.value >= 0 ? 'deposit' : 'withdrawal';
+    this.date = new Date(date);
   }
 }
