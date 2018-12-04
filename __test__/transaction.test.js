2161bf6ca42d16d198fc36fa63d60764dfd87bb8
Chris Cooksley <christopher.cooksley@gmail.com>
2018-12-04 08:40:16 +0000

    Removed comment from test

--- __test__/transaction.test.js
+++ __test__/transaction.test.js
@@ -7,7 +7,6 @@ describe('Transaction', () => {
       expect(transaction.credit).toBe(500);
       expect(transaction.debit).toBe(0);
       expect(transaction.date).toEqual(new Date('2018-12-25'));
-      // expect(transaction.date.toDateString()).toBe(new Date().toDateString());
     });
   });
 });
